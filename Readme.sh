25b9c252a2820e4ffd968a2e5d2863442513cb7b
JainSkariahThomas <50365485+JainSkariahThomas@users.noreply.github.com>
2022-06-23 16:35:40 +0530

    Update Readme.sh

---
+++ Readme.sh
@@ -0,0 +1,6 @@
+Trying to Build up a entire bash script that will help to deploy a server with all required installation and application as needed.
+
+Second, Try to build up a terraform and ansible code with same setup
+
+
+Still work on progress
